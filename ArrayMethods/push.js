f88c9f56fd007ce413195f1a197e901ecf301ccd
LuxianAiolos <luxianaiolos@gmail.com>
2022-12-17 21:00:45 -0800

    anagrams

---
+++ ArrayMethods/push.js
@@ -0,0 +1,3 @@
+const arr = [1,2,3]
+console.log(arr.unshift(1))
+console.log(arr.pop())
---
+++ neetcode/groupAnagrams.js
@@ -0,0 +1,13 @@
+const strs = ["eat","tea","tan","ate","nat","bat"]
+
+const groupAnagrams = (strs) => {
+  // console.log(strs)
+  const sortedStrings = strs.map(str => 
+    str.split('').sort().join('')
+  )
+  
+  sortedStrings
+  return 
+}
+
+console.log(groupAnagrams(strs))
