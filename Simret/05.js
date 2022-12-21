76ba8a6982f380f1537687a5b6d9cad87ff60d3f
LuxianAiolos <luxianaiolos@gmail.com>
2022-12-20 21:47:01 -0800

    Find all substrings

---
+++ Simret/05.js
@@ -0,0 +1,15 @@
+// 3. Write a JavaScript function that generates all combinations of a string. 
+// Example string: 'dog' 
+// Expected Output: d, do, dog, o, og, g 
+
+
+//DO THIS EXERCISE TOMORROW!!!!!
+
+let string = 'dog'
+
+const stringCombos = (str) => {
+  const currentStr = str[0] += str[2] 
+  return currentStr
+}
+
+console.log(stringCombos(string))
