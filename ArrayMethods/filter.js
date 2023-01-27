cf1a35947490636c7cbebcc3806a0a4f01a452b4
LuxianAiolos <luxianaiolos@gmail.com>
2023-01-26 17:19:01 -0800

    Update filter.js

--- ArrayMethods/filter.js
+++ ArrayMethods/filter.js
@@ -1,3 +1,10 @@
  // given an input of numbers, return the counts of odd and even numbers
 
- 
+ const nums = [1, 4, 5, 6, 7, 8, 10]
+
+ const lessThan7 = (arr) => {
+  const lessThan = arr.filter(n => n < 7)
+  return lessThan
+ }
+
+ console.log(lessThan7(nums))
