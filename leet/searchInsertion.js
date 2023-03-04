929c09705b12273c11aa264f6285461f0e317376
LuxianAiolos <luxianaiolos@gmail.com>
2023-03-03 16:29:11 -0800

    Search insertion

---
+++ leet/searchInsertion.js
@@ -0,0 +1,26 @@
+var searchInsert = function(nums, target) {
+  // let x = target - 1
+  // if (target > nums.length) {
+  //   x = nums.length - 1
+  // }
+
+  // for( let i = 0; i < nums.length; i++ ) {
+  //   console.log(nums[x], target, x )
+  //   // if nums[x] === x - 2 return x+1
+  //   // if nums[x] === x + 2 return x-1
+  //   if ( nums[x] > target ) {
+  //     x--
+  //   } else if ( nums[i] < target ) {
+  //     x++
+  //   } else if(nums[x] === x - 2) {return x + 1, '1'}
+  //     else if(nums[x] === x + 2) {return x - 1, '2'}
+  //     else {
+  //     // console.log(x)
+  //     return x + ' result'
+  //   }
+  // }
+
+  
+};
+
+console.log(searchInsert([1, 3, 4, 5], 2))
