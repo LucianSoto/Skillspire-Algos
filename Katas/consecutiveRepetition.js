27480ad4bd53eb4812621f34e9e3132161ed821f
LuxianAiolos <luxianaiolos@gmail.com>
2023-03-21 13:27:12 -0700

    Finished Algo

--- Katas/consecutiveRepetition.js
+++ Katas/consecutiveRepetition.js
@@ -1,28 +1,22 @@
 function longestRepetition(s) {
   if(s === '') return ["",0];
   let counter = 1
+  let lettersArr = []
   let countsArr = []
+  let result = []
 
   for( let i = 0; i < s.length; i++ ) {
     let char = s[i]
 
     if(char === s[i + 1]) {
       counter++
-      console.log(true, char, counter)
     } else {
-      console.log(counter, char)
-      // push prev to array 
-      // then set new char
-      // then reset counter 
-      countsArr.push([char, counter])
+      lettersArr.push(char)
+      countsArr.push(counter)
       counter = 1
       char = s[i + 1]
     }
   }
-
-  // countsArr.map((arr, i) => {
-  //   let max = 
-  // })
 
   for( let i = 0; i < countsArr.length; i++ ) {
     let highest = countsArr[0][1]
@@ -30,12 +24,14 @@ function longestRepetition(s) {
     if(current > highest) {
       highest = countsArr[i][1]
     }
-    console.log(highest, 'highest')
   }
 
-  console.log(countsArr, 'counts arr')
-  return countsArr
+  const highestCount = Math.max(...countsArr)
+  const hcIndex = countsArr.indexOf(highestCount)
+
+  result.push(lettersArr[hcIndex], highestCount)
+  return result
 }
 
 
-longestRepetition('bbaaa')
+console.log(longestRepetition('bbaaacdddd'))
