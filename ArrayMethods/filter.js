392adb99673eda8821198de9c102711dd016007c
LuxianAiolos <luxianaiolos@gmail.com>
2023-03-29 12:19:55 -0700

    Linked List

--- ArrayMethods/filter.js
+++ ArrayMethods/filter.js
@@ -1,10 +1,19 @@
  // given an input of numbers, return the counts of odd and even numbers
 
- const nums = [1, 4, 5, 6, 7, 8, 10]
+//  const nums = [1, 4, 5, 6, 7, 8, 10]
 
- const lessThan7 = (arr) => {
-  const lessThan = arr.filter(n => n < 7)
-  return lessThan
- }
+//  const lessThan7 = (arr) => {
+//   const lessThan = arr.filter(n => n < 7)
+//   return lessThan
+//  }
 
- console.log(lessThan7(nums))
+//  console.log(lessThan7(nums))
+
+let array = [1, 2, 3, 45, 6]
+
+// console.log(array.filter(num => num > 7))
+// console.log(array.filter(num => num % 2 === 0))
+
+
+
+
