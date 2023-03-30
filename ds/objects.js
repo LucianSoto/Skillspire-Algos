ac70069c2b2dc1621a6cc54a6b49a15c1887101b
LuxianAiolos <luxianaiolos@gmail.com>
2023-03-30 01:13:19 -0700

    Data Structures

---
+++ ds/objects.js
@@ -0,0 +1,18 @@
+const data = {
+  haircuts: [
+    { 
+      title: 'cut', 
+      desc: 'yes',
+    },
+    {
+      title: 'morecuts',
+      desc: 'yes again',
+    },
+  ],
+  dyes: [
+    {
+      title: 'hair dye',
+      desc: 'mhmm',
+    },
+  ]
+}
