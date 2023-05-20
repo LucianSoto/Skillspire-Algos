00d5c60c9d7005bd4688045f33163d79851a891a
Lucian Aiolos <luxianaiolos@gmail.com>
2023-05-20 11:02:18 -0700

    reduce without initial value
    when passed an object to iterate over and no initial value is set it will just return the object.

---
+++ FCC/reduce.js
@@ -0,0 +1,10 @@
+
+
+const users = [
+  { name: 'John', age: 34 },
+  // { name: 'Amy', age: 20 },
+  // { name: 'camperCat', age: 10 }
+];
+
+const sumOfAges = users.reduce((sum, user) => sum + user.age);
+console.log(sumOfAges);
