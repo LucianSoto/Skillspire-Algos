2dbb7d720f45e119e1503f4026bb61ddc36088a2
Lucian Aiolos <luxianaiolos@gmail.com>
2023-05-20 11:12:53 -0700

    obj[] create an object inside the reducer funciton

--- FCC/reduce.js
+++ FCC/reduce.js
@@ -1,10 +1,29 @@
 
 
+// const users = [
+//   { name: 'John', age: 34 },
+//   // { name: 'Amy', age: 20 },
+//   // { name: 'camperCat', age: 10 }
+// ];
+
+// const sumOfAges = users.reduce((sum, user) => sum + user.age);
+// console.log(sumOfAges);
+
 const users = [
   { name: 'John', age: 34 },
-  // { name: 'Amy', age: 20 },
-  // { name: 'camperCat', age: 10 }
+  { name: 'Amy', age: 20 },
+  { name: 'camperCat', age: 10 }
 ];
 
-const sumOfAges = users.reduce((sum, user) => sum + user.age);
-console.log(sumOfAges);
+                      /////////does obj here or down there create an object?
+const usersObj = users.reduce((obj, user) => {
+  console.log(obj[0])
+  console.log(obj)
+
+  // obj[] creates a new object 
+  obj[user.name] = user.age;
+  return obj;
+}, {});
+console.log(usersObj, 'result')
+
+console.log(users[0], 'test')
